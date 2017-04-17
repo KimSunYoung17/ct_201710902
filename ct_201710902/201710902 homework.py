be7ed4160f2599b20e5fb1f834071160c3abf6ea
KimSunYoung17 <kimsy062264@hanmail.net>
2017-04-17 15:53:11 +0900

    Add files via upload

---
+++ ct_201710902/201710902 homework.py
@@ -0,0 +1,10 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> wn = turtle.Screen()
+>>> t1 = turtle.Turtle()
+>>> wn.addshape("C:\\Users\\SeonYoungKim\\Documents\\201710902.gif")
+>>> t1.shape("C:\\Users\\SeonYoungKim\\Documents\\201710902.gif")
+>>> t1.fd(25)
+>>> t1.fd(50)
+>>> 
