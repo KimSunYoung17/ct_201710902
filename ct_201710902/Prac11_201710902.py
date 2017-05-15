a82c14d2cfef0c34fba3a370dbb95056f62d1aa5
KimSunYoung17 <kimsy062264@hanmail.net>
2017-05-15 17:44:28 +0900

    Add files via upload

---
+++ ct_201710902/Prac11_201710902.py
@@ -0,0 +1,24 @@
+﻿
+from PIL import Image
+
+m = Image.open("C:\\Users\\SeonYoungKim\\Downloads\\italy-1587287_1920.jpg")
+
+m.show()
+
+get_ipython().magic(u'matplotlib inline')
+from matplotlib.pyplot import imshow
+imshow(m)
+
+
+
+from PIL import Image
+m = Image.open("C:\\Users\\SeonYoungKim\\Downloads\\italy-1587287_1920.jpg")
+m.save("C:\\Users\\SeonYoungKim\\Downloads\\italy-1587287_1920.gif")
+
+
+m.show()
+
+get_ipython().magic(u'matplotlib inline')
+from matplotlib.pyplot import imshow
+imshow(m)
+
