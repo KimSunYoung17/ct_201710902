e736b9edccc99c7a5fffed40cc1c7ed69775e9f8
KimSunYoung17 <kimsy062264@hanmail.net>
2017-05-22 17:05:58 +0900

    Add files via upload

---
+++ ct_201710902/mieumAt_201710902.py
@@ -0,0 +1,61 @@
+﻿
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+
+def giyukAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    t1.fd(size)
+    t1.rt(90) 
+    t1.fd(size)
+
+giyukAt(-100,100,100)
+
+
+
+t1.home()
+
+t1.clear()
+
+
+def nieunAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    t1.rt(90)
+    t1.fd(size)
+    t1.lt(90)
+    t1.fd(size)
+
+
+
+nieunAt(-100,100,100)
+
+
+
+t1.home()
+
+t1.clear()
+
+def mieumAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    giyukAt(x,y,size)
+    t1.penup()
+    t1.goto(x,y)
+    t1.lt(90)
+    t1.pendown()
+    nieunAt(x,y,size)
+
+
+mieumAt(100,-100,100)
+
+
+
+
+
+
